2b1d07c3010f7e8f2047312671a949e1d6c0ba15
MB Shihab Aaqil Ahamed <shihabaaqilahamed@gmail.com>
2023-10-27 17:22:45 +0530

    Single Shot Multi-Box Detector

---
+++ models/ssmd.py
@@ -0,0 +1,94 @@
+def conv_layer(filter,kernel_size,
+              layer,strides=1,
+              padding='same',
+              activation='linear',pool=False,
+              poolsize=2,poolstride=2,conv=True):
+  if conv == True:
+      layer = tf.keras.layers.Conv2D(filters=filter,
+                                  kernel_size=kernel_size,
+                                  strides=strides,
+                                  activation=activation,
+                                  padding=padding,
+                                  kernel_initializer='he_normal')(layer)
+      layer = tf.keras.layers.BatchNormalization()(layer)
+      layer = tf.keras.layers.ReLU()(layer)
+  elif pool == True:
+    layer=tf.keras.layers.MaxPool2D(pool_size=(poolsize,poolsize),
+                                    strides=poolstride,padding='same')(layer)
+  return layer
+
+
+def SKU110K_model():
+  outputs=[]
+  densenet_121 = tf.keras.applications.DenseNet121(
+                                            input_shape=(hImage, wImage, 3),
+                                            include_top=False)
+
+  #Feature Layer 1
+
+  layer = densenet_121.get_layer('pool3_relu').output
+  output = tf.keras.layers.Conv2D(filters=4*(4+nClasses+1),
+            kernel_size=3,
+            padding='same',
+            kernel_initializer='glorot_normal')(layer)
+  output = tf.keras.layers.Reshape([-1, 4+nClasses+1])(output)
+  outputs.append(output)
+
+  #Feature Layer 2
+
+  layer = densenet_121.get_layer('pool4_relu').output
+  output = tf.keras.layers.Conv2D(filters=6*(4+nClasses+1),
+            kernel_size=3,
+            padding='same',
+            kernel_initializer='glorot_normal')(layer)
+  output = tf.keras.layers.Reshape([-1, 4+nClasses+1])(output)
+  outputs.append(output)
+
+
+  #Feature Layer 3
+
+  layer = densenet_121.get_layer('relu').output
+  output = tf.keras.layers.Conv2D(filters=6*(4+nClasses+1),
+          kernel_size=3,
+          padding='same',
+          kernel_initializer='glorot_normal')(layer)
+  output = tf.keras.layers.Reshape([-1, 4+nClasses+1])(output)
+  outputs.append(output)
+
+  #Feature Layer 4
+
+  layer = conv_layer(128, 1, layer)
+  layer = conv_layer(256, 3, layer, strides=2)
+  output = tf.keras.layers.Conv2D(filters=6*(4+nClasses+1),
+          kernel_size=3,
+          padding='same',
+          kernel_initializer='glorot_normal')(layer)
+  output = tf.keras.layers.Reshape([-1, 4+nClasses+1])(output)
+  outputs.append(output)
+
+  #Feature Layer 5
+
+  layer = conv_layer(128, 1, layer,padding= 'valid')
+  layer = conv_layer(256, 3, layer,padding= 'valid')
+  output = tf.keras.layers.Conv2D(filters=4*(4+nClasses+1),
+          kernel_size=3,
+          padding='same',
+          kernel_initializer='glorot_normal')(layer)
+  output = tf.keras.layers.Reshape([-1, 4+nClasses+1])(output)
+  outputs.append(output)
+
+  #Feature Layer 6
+
+  layer = conv_layer(128, 1, layer,padding= 'valid')
+  layer = conv_layer(256, 3, layer,padding= 'valid')
+  output = tf.keras.layers.Conv2D(filters=4*(4+nClasses+1),
+        kernel_size=3,
+        padding='same',
+        kernel_initializer='glorot_normal')(layer)
+  output = tf.keras.layers.Reshape([-1, 4+nClasses+1])(output)
+  outputs.append(output)
+
+  out = tf.keras.layers.Concatenate(axis=1)(outputs)
+  model = tf.keras.models.Model(densenet_121.input,out, name='SKU110K_model')
+  model.summary()
+  return model
